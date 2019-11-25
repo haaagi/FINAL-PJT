5cc21d060708a2101c45daea9e0c50c6fa25132d
hagi <ceajer@gmail.com>
2019-11-25 20:00:34 +0900

    navbar tuning

--- lastproject_front_end/src/views/Home.vue
+++ lastproject_front_end/src/views/Home.vue
@@ -4,7 +4,8 @@
     <div>
       <ul>
         <li v-for="movie in movies" :key="movie.id">
-          {{ movie.title }}
+          <img :src="movie.poster_url" :alt="movie.title">
+          <p>{{movie.title}}</p>
         </li>
       </ul>
     </div>
@@ -14,12 +15,11 @@
 </template>
 
 <script>
-// @ is an alias to /src
+
 // import router from '../router';
 const axios = require('axios'); 
 // import MovieHome from '../components/MovieHome';
 import navbar from '../components/navbar';
-
 
 export default {
   name: 'home',
@@ -31,7 +31,6 @@ export default {
   components: {
     navbar,
   },
-
   created () {
     axios.get('http://localhost:8000/api/movies/')
       .then(res=> this.movies = res.data)
