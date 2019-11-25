f202d87653b19c1bf1c733a3bc3e687fb79b76e0
hagi <ceajer@gmail.com>
2019-11-25 15:57:53 +0900

    review serializer

--- LastProject/movies/serializers.py
+++ LastProject/movies/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import Review
 
 
-User = get_user_model()
 
+class ReviewSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(required=True)
+    class Meta:
+        model = Review
+        fields = ('id','user','movie','content','score',)
