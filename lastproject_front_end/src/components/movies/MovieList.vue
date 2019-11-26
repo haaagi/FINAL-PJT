44fdf2635098c6bf3ce0134c58e500738b0d5a62
sooya <soosmile9653@gmail.com>
2019-11-26 10:53:29 +0900

    movie list edit

--- lastproject_front_end/src/components/movies/MovieList.vue
+++ lastproject_front_end/src/components/movies/MovieList.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
         <h1>MovieList</h1>
-        <ul>
+     <b-card-group deck>
+    <div v-for="movie in movies" :key="movie.id" class="col-3 my-3">
+ 
+    <b-card title="Title" :img-src="movie.poster_url" :img-alt="movie.title" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.
+      </b-card-text>
+    </b-card>
+  
+</div>
+</b-card-group> 
+
+
+
+        <!-- <ul>
             <li v-for="movie in movies" :key="movie.id">
                 <img :src="movie.poster_url" :alt="movie.title">
             </li>
-        </ul>  
+        </ul>   -->
     </div>
 </template>
 
@@ -19,7 +34,7 @@ export default {
         }
     },
     props: {
-        Movies: Array,
+        movies: Array,
     }
 }
 </script>
