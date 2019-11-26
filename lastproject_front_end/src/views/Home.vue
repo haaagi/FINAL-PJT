e0e6b0b51c6fb416310a86c5c018767ee0f286ab
sooya <soosmile9653@gmail.com>
2019-11-26 10:21:22 +0900

    home.app edit

--- lastproject_front_end/src/views/Home.vue
+++ lastproject_front_end/src/views/Home.vue
@@ -1,6 +1,43 @@
 <template>
-  <div class="home">
-    <div>
+
+<div class="home">
+<div class="scene scene--card">
+  <div class="card">
+    <div class="card__face card__face--front">front</div>
+    <div class="card__face card__face--back">back</div>
+  </div>
+</div>
+<p>Click card to flip.</p>
+
+</div>
+
+
+  <!-- <div class="home">
+   <b-card-group>
+<div v-for="movie in movies" :key="movie.id">
+  <b-card
+    :title="movie.title"
+    :img-src="movie.poster_url"
+    :img-alt="movie.title"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+    img-width="100"
+  
+  >
+
+    <b-button href="#" variant="primary">Go somewhere</b-button>
+  </b-card>
+
+</div>
+  </b-card-group>
+</div> -->
+
+
+
+
+    <!-- <div>
       <ul>
         <li v-for="movie in movies" :key="movie.id">
           <img :src="movie.poster_url" :alt="movie.title">
@@ -9,8 +46,7 @@
       </ul>
     </div>
 
-
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -19,6 +55,10 @@
 const axios = require('axios'); 
 // import MovieHome from '../components/MovieHome';
 
+// var card = document.querySelector('.card');
+// card.addEventListener( 'click', function() {
+//   card.classList.toggle('is-flipped');
+// });
 
 export default {
   name: 'home',
@@ -39,3 +79,49 @@ export default {
 
 </script>
 
+<style>
+body { font-family: sans-serif; }
+
+.scene {
+  width: 200px;
+  height: 260px;
+  border: 1px solid #CCC;
+  margin: 40px 0;
+  perspective: 600px;
+}
+
+.card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  transform-style: preserve-3d;
+  transform-origin: center right;
+  transition: transform 1s;
+}
+
+.card.is-flipped {
+  transform: translateX(-100%) rotateY(-180deg);
+}
+
+.card__face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  line-height: 260px;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+  font-size: 40px;
+  backface-visibility: hidden;
+}
+
+.card__face--front {
+  background: red;
+}
+
+.card__face--back {
+  background: blue;
+  transform: rotateY(180deg);
+}
+</style>
